9cfa2ce810ed35f73910470eaa93f649d657d25c
theLife10 <jdgarcia9@miners.utep.edu>
2019-02-04 17:30:12 -0800

    restructured intents

--- README1.md
+++
@@ -1,9 +0,0 @@
-All files are working correctly and connected <br/>
-add_history(): helped by michael contraras. 
-tokenize(): helped by michael contraras.
-Makefile is working. <br/>
-In order to run the file: <br/>
-To clean type: make clean <br/>
-To compile type : make <br/>
-to run type: ./command <br/>
-
--- jgarcia_prog1.c
+++ jgarcia_prog1.c
@@ -3,17 +3,17 @@
 #include  <stdlib.h>
 #include <string.h>
 int main(){
-	char word[100];
-	while(1){
-		printf("$ ");
-		fgets(word,100,stdin);
-		 if(word[0] == 'b'){
-			free_tokens(argtok(word));
-			break;
-		}
-		else{
-			print_tokens(argtok(word));
-}
-	}
+    char word[100];
+    while(1){
+        printf("$ ");
+        fgets(word,100,stdin);
+        if(word[0] == 'b'){
+            free_tokens(argtok(word));
+            break;
+        }
+        else{
+            print_tokens(argtok(word));
+        }
+    }
 }
 
